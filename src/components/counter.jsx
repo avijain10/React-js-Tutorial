9ce1d7035ba81482c038d456a25090e44c28abf1
Avi Jain <avijain1001@gmail.com>
2020-06-01 23:49:11 +0530

    Updated:Few files

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -9,7 +9,19 @@ class Counter extends Component {
     this.setState({ count: this.state.count + 1 });
   };
 
+  componentDidUpdate(prevProps, prevState) {
+    console.log("prevProps", prevProps);
+    console.log("prevState", prevState);
+    if (prevProps.counter.value !== this.props.counter.value) {
+    }
+  }
+
+  componentWillUnmount() {
+    console.log("Counter - Unmount");
+  }
+
   render() {
+    console.log("Counter - Rendered");
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
